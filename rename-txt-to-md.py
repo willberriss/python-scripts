b9e740a7f0244e9674440ab94c5bdd577b95dece
Will Berriss <will.berriss@gmail.com>
2017-03-28 08:41:55 +0100

    Improve comment and explicitly use python3

--- rename-txt-to-md.py
+++ rename-txt-to-md.py
@@ -1,8 +1,8 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 import glob, os
 
 for fname in glob.glob('*txt*'):
-    # Replace all occurrences of txt with md
+    # e.g. replace fred.txt.bak with fred.md.bak
     os.rename(fname, fname.replace('txt', 'md'))
 
