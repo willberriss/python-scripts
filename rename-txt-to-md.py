54f7b9d3ee73fa829026c34da7faa0aa800b5ba2
Will Berriss <will.berriss@gmail.com>
2017-03-28 09:26:44 +0100

    Stick to PEP 8 style guide for imports. Improve variable name and comment.

--- rename-txt-to-md.py
+++ rename-txt-to-md.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python3
 
-import glob, os
+import glob
+import os
 
-for fname in glob.glob('*txt*'):
-    # e.g. replace fred.txt.bak with fred.md.bak
-    os.rename(fname, fname.replace('txt', 'md'))
+# e.g. replace fred.txt.bak with fred.md.bak
+for filename in glob.glob('*txt*'):
+    os.rename(filename, filename.replace('txt', 'md'))
 
